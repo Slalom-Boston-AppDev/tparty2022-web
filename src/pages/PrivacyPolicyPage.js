9e130d5e7e2531387ce69e7e3947d8c0d8b4278a
Eric Jiang <eric.jiang@slalom.com>
2022-07-27 11:43:31 -0400

    Using React Markdown for Privacy Policy. Just need to add content to PrivacyPolicy.md and save to reflect changes

--- src/pages/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicyPage.js
@@ -1,3 +1,20 @@
-export default function PrivacyPolicyPage() {
-    return <h1>Privacy Policy</h1>
+import { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import Policy from "../PrivacyPolicy.md";
+
+const PrivacyPolicyPage = () => {
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+        fetch(Policy)
+        .then((res) => res.text())
+        .then((text) => setContent(text));
+    }, []);
+    return (
+        <div className='policy'>
+            <ReactMarkdown children={content} />
+        </div>
+    )
 }
+
+export default PrivacyPolicyPage;
