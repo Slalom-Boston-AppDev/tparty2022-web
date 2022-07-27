547984f8e73f90150dd7c8ab084664d14778d614
Eric Jiang <eric.jiang@slalom.com>
2022-07-27 10:09:55 -0400

    Navbar implemented, gh pages setup

--- privacy-policy/src/index.js
+++ privacy-policy/src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
--- privacy-policy/src/pages/HomePage.js
+++ privacy-policy/src/pages/HomePage.js
@@ -1,3 +1,5 @@
 export default function HomePage() {
-    return <h1>Home</h1>
+    return (
+        <h1>This is Slalom Boston's TParty app at their annual retreat</h1>
+    )
 }
